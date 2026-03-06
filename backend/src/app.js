b23a677dac34b894c3d99f60c1da5dddb9aaa547
tameem akhtar <akhtartamimm@gmail.com>
2026-03-06 07:57:39 +0530

    update api urls and favicon

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ let app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://moodify-ny2y.onrender.com'',
     credentials: true,
 }))
 app.use(cookieparser())
